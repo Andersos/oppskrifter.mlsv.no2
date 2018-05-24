7744856e8564f046fa9d8950e0de0610be9219ce
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2018-05-24 23:29:01 +0200

    Update Gatsby create page

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,8 +2,8 @@ const _ = require('lodash')
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
-exports.createRecipes = ({ boundActionCreators, graphql }) => {
-  const { createRecipe } = boundActionCreators
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
 
   return graphql(`
     {
