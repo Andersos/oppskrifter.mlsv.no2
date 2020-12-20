d287f81290526f89f68ff875aa2df38c45ca5d25
Anders Olsen Sandvik <288977+Andersos@users.noreply.github.com>
2020-12-20 12:51:53 +0100

    Delete cms.js

--- src/cms/cms.js
+++
@@ -1,8 +0,0 @@
-import CMS from 'netlify-cms'
-
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import RecipePreview from './preview-templates/RecipePreview'
-
-CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('recipe', RecipePreview)
